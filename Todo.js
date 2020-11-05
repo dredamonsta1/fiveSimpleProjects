2ad78e4e86c0ce0cfc125f12c5f23b92e82250c4
andre <andre_w3@yahoo.com>
2020-11-04 21:21:04 -0500

    append x to delete list item

--- Todo.js
+++ Todo.js
@@ -1,10 +1,29 @@
 const todoFunction = () => {
     // const todo = document.getElementsByClassName("todo")
     let todo = document.getElementById("todoVal").value;
+    // let displayVal = document.querySelector
+
+    let myTodoList = document.getElementsByTagName("LI");
+    let i;
+    for (i = 0; i < myTodoList.length; i++) {
+        let span = document.createElement("SPAN");
+        let closeX = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(closeX);
+        myTodoList[i].appendChild(span);
+    }
+
 
     console.log(todo, "hello todo");
 }
 
+
+
+
+
+
+
 const showValueFunction = () => {
+    let todoValue = document.querySelector()
 
 }
