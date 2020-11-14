f320b30aaed3f52e5de476c9a67c826f18cc0b48
andre <andre_w3@yahoo.com>
2020-11-14 02:28:05 -0500

    appending words to list

--- Todo.js
+++ Todo.js
@@ -1,29 +1,66 @@
-const todoFunction = () => {
-    // const todo = document.getElementsByClassName("todo")
-    let todo = document.getElementById("todoVal").value;
-    // let displayVal = document.querySelector
+// const todoFunction = () => {
 
-    let myTodoList = document.getElementsByTagName("LI");
-    let i;
-    for (i = 0; i < myTodoList.length; i++) {
-        let span = document.createElement("SPAN");
-        let closeX = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(closeX);
-        myTodoList[i].appendChild(span);
-    }
+//     let todo = document.getElementById("todoVal").value;
 
 
-    console.log(todo, "hello todo");
+//     console.log(todo, "hello todo");
+
+
+// }
+
+var myTodoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myTodoList.length; i++) {
+    var span = document.createElement("SPAN");
+    var closeX = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(closeX);
+    myTodoList[i].appendChild(span);
+
 }
 
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onClick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
 
 
+function todoFunction() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("todoVal").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("oh no what are you doing!");
+    } else {
+        document.getElementById("theList").appendChild(li);
+    }
+    document.getElementById("todoVal").value = "";
+    var span = document.createTextNode("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(closeX);
+    li.appendChild(span);
 
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+    console.log("hello")
 
-
-
-const showValueFunction = () => {
-    let todoValue = document.querySelector()
+    // let todoValue = document.querySelector()
 
 }
